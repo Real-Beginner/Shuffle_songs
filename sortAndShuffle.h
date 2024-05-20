ded41067ccfc290ffc7b1dc9fdf9abd303acfd3e
Real-Beginner <146841402+Real-Beginner@users.noreply.github.com>
2024-05-20 22:48:28 +0100

    Create sortAndShuffle.h

---
+++ sortAndShuffle.h
@@ -0,0 +1,6 @@
+void sortArtists(char sortedArtists[][80] , int numOfArtists);
+
+void sortSongs(char songsOfAnArtist[][80], int numOfArtists);
+
+void shuffleSongs(char array[][163], int numOfSongs);
+
